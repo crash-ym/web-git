a03ab2c3f389046cd13159caf0238edc7dcb5844
crash-ym <yangm1022@163.com>
2018-12-20 19:48:32 +0800

    webgit

---
+++ src/test/java/com/bw/ae/wb_admintest/web_ym/AppTest.java
@@ -0,0 +1,13 @@
+package com.bw.ae.wb_admintest.web_ym;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
